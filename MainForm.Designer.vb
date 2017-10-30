190d09a6dd23fed96792dd80a4f84af206dacaa1
André Obelink <andre@obelink.com>
2017-10-30 18:43:05 +0100

    Initial commit

---
+++ MainForm.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SelectFileButton = New System.Windows.Forms.Button()
+        Me.ImagePictureBox = New System.Windows.Forms.PictureBox()
+        Me.ResultTextBox = New System.Windows.Forms.TextBox()
+        Me.VisionTypeComboBox = New System.Windows.Forms.ComboBox()
+        CType(Me.ImagePictureBox,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
+        '
+        'SelectFileButton
+        '
+        Me.SelectFileButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.SelectFileButton.Location = New System.Drawing.Point(800, 407)
+        Me.SelectFileButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.SelectFileButton.Name = "SelectFileButton"
+        Me.SelectFileButton.Size = New System.Drawing.Size(94, 22)
+        Me.SelectFileButton.TabIndex = 2
+        Me.SelectFileButton.Text = "Select file..."
+        Me.SelectFileButton.UseVisualStyleBackColor = true
+        '
+        'ImagePictureBox
+        '
+        Me.ImagePictureBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
+            Or System.Windows.Forms.AnchorStyles.Left)  _
+            Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.ImagePictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.ImagePictureBox.Location = New System.Drawing.Point(13, 43)
+        Me.ImagePictureBox.Name = "ImagePictureBox"
+        Me.ImagePictureBox.Size = New System.Drawing.Size(434, 353)
+        Me.ImagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.ImagePictureBox.TabIndex = 3
+        Me.ImagePictureBox.TabStop = false
+        '
+        'ResultTextBox
+        '
+        Me.ResultTextBox.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
+            Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.ResultTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.ResultTextBox.Location = New System.Drawing.Point(453, 43)
+        Me.ResultTextBox.Multiline = true
+        Me.ResultTextBox.Name = "ResultTextBox"
+        Me.ResultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.ResultTextBox.Size = New System.Drawing.Size(441, 353)
+        Me.ResultTextBox.TabIndex = 4
+        '
+        'VisionTypeComboBox
+        '
+        Me.VisionTypeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.VisionTypeComboBox.FormattingEnabled = true
+        Me.VisionTypeComboBox.Location = New System.Drawing.Point(13, 12)
+        Me.VisionTypeComboBox.Name = "VisionTypeComboBox"
+        Me.VisionTypeComboBox.Size = New System.Drawing.Size(434, 21)
+        Me.VisionTypeComboBox.TabIndex = 6
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(907, 440)
+        Me.Controls.Add(Me.VisionTypeComboBox)
+        Me.Controls.Add(Me.ResultTextBox)
+        Me.Controls.Add(Me.ImagePictureBox)
+        Me.Controls.Add(Me.SelectFileButton)
+        Me.Name = "MainForm"
+        Me.Text = "Micosoft Cognitive Services with VB - Part 2"
+        CType(Me.ImagePictureBox,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents SelectFileButton As Button
+    Friend WithEvents ImagePictureBox As PictureBox
+    Friend WithEvents ResultTextBox As TextBox
+    Friend WithEvents VisionTypeComboBox As ComboBox
+End Class
---
+++ MainForm.vb
@@ -0,0 +1,186 @@
+﻿Imports System.IO
+Imports System.Text
+Imports Microsoft.ProjectOxford.Vision
+Imports Microsoft.ProjectOxford.Vision.Contract
+
+Public Class MainForm
+
+    Private Readonly _visionServiceClient As New VisionServiceClient("17e72298c40a4532901b20c1df72c5b2", 
+                                                                     "https://westeurope.api.cognitive.microsoft.com/vision/v1.0")
+
+    Private _facesAnalysisResult As AnalysisResult
+
+    Private Async Function GetAnalysisResultAsync(imageFilePath As String) As Task(Of AnalysisResult)
+
+        Dim visualFeatures() =  {VisualFeature.Adult, VisualFeature.Categories, VisualFeature.Color, 
+                                 VisualFeature.Description, VisualFeature.Faces, VisualFeature.ImageType, VisualFeature.Tags}
+
+        Using imageFileStream  = File.OpenRead(imageFilePath)
+            Return Await _visionServiceClient.AnalyzeImageAsync(imageFileStream, visualFeatures)
+        End Using
+
+    End Function
+
+    Private Async Function GetHandwritingRecognitionOperationResultAsync(imageFilePath As String) As Task(Of HandwritingRecognitionOperationResult)
+
+        Using imageFileStream  = File.OpenRead(imageFilePath)
+
+            Dim operation = Await  _visionServiceClient.CreateHandwritingRecognitionOperationAsync(imageFileStream)
+            Dim result = New  HandwritingRecognitionOperationResult() 
+
+            While result.Status = HandwritingRecognitionOperationStatus.Running OrElse 
+                  result.Status = HandwritingRecognitionOperationStatus.NotStarted
+                result =  Await _visionServiceClient.GetHandwritingRecognitionOperationResultAsync(operation)
+                Await Task.Delay(100)
+            End While
+            
+            Return result
+        End Using
+
+    End Function
+
+    Private Async Function GetOcrResultsAsync(imageFilePath As String) As Task(Of OcrResults)
+        
+        Using imageFileStream  = File.OpenRead(imageFilePath)
+            Return Await _visionServiceClient.RecognizeTextAsync(imageFileStream, "unk", True)
+        End Using
+   
+    End Function
+    
+    Private Function GetImageFilePath() As String
+
+        Dim dialog As New OpenFileDialog
+        With dialog
+            .Filter = "jpg files (*.jpg)|*.jpg|All files (*.*)|*.*"
+            Return If(.ShowDialog = DialogResult.OK, .FileName, String.Empty)
+        End With
+  
+    End Function
+
+    Private Function ProcessAnalysisResult(result As AnalysisResult) As String
+
+        With result.Description.Captions.FirstOrDefault()
+            Return $"{.Text} - {(.Confidence * 100) .ToString("N2")}%"
+        End With
+        
+    End Function
+
+    Private Function ProcessHandwritingRecognitionOperationResult(result As HandwritingRecognitionOperationResult) As String
+        
+        If result.Status = HandwritingRecognitionOperationStatus.Succeeded Then
+
+            Dim stringBuilder As New StringBuilder
+
+            For Each line In result.RecognitionResult.Lines
+                For Each word In line.Words
+                    stringBuilder.Append(word.Text).Append(" ")
+                Next
+                stringBuilder.AppendLine()
+            Next
+
+            Return stringBuilder.ToString()
+        Else
+            Return $"Error; Status: {result.Status}"
+        End If
+
+    End Function
+    
+    Private Function ProcessOcrResults(result As OcrResults) As String
+
+        Dim stringBuilder As New StringBuilder
+
+        For Each ocrRegion In result.Regions
+            For Each line In ocrRegion.Lines
+                For Each word In line.Words
+                    stringBuilder.Append(word.Text).Append(" ")
+                Next
+                stringBuilder.AppendLine()
+            Next
+            stringBuilder.AppendLine()
+        Next
+
+        MessageBox.Show($"Language: {result.Language}")
+
+        Return stringBuilder.ToString()
+
+    End Function
+
+    Private Async Sub SelectFileButton_Click(sender As Object, e As EventArgs) Handles SelectFileButton.Click
+
+        Dim imageFilePath = GetImageFilePath()
+
+        If imageFilePath <> String.Empty Then
+            ImagePictureBox.ImageLocation = imageFilePath
+
+            _facesAnalysisResult = Nothing
+            ResultTextBox.Text = "Processing..."
+
+            Select Case VisionTypeComboBox.SelectedIndex
+                Case 0  'Analyze image
+                    Dim result = Await GetAnalysisResultAsync(imageFilePath)
+                    ResultTextBox.Text = ProcessAnalysisResult(result)
+                Case 1  'Analyze image (Faces)
+                    _facesAnalysisResult = Await GetAnalysisResultAsync(imageFilePath)
+                    ResultTextBox.Text = ProcessAnalysisResult(_facesAnalysisResult)
+                    ImagePictureBox.Invalidate()
+                Case 2  'Recognize text
+                    Dim result = Await GetOcrResultsAsync(imageFilePath)
+                    ResultTextBox.Text = ProcessOcrResults(result)
+                Case 3  'Recognize handwriting
+                    Dim result = Await GetHandwritingRecognitionOperationResultAsync(imageFilePath)
+                    ResultTextBox.Text = ProcessHandwritingRecognitionOperationResult(result)
+            End Select
+            
+        End If
+
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        With VisionTypeComboBox
+            .Items.Add("Analyze image")
+            .Items.Add("Analyze image (face)")
+            .Items.Add("Recognize text")
+            .Items.Add("Recognize handwriting")
+            .SelectedIndex = 0
+        End With
+
+    End Sub
+
+    Private Sub ImagePictureBox_Paint(sender As Object, e As PaintEventArgs) Handles ImagePictureBox.Paint
+
+        If _facesAnalysisResult?.Faces?.Any() Then
+            DrawFaces(e)
+        End If
+
+    End Sub
+
+    Private Sub DrawFaces(e As PaintEventArgs)
+      
+        ' Calculate ScaleFactor
+        Dim scaleFactorX = ImagePictureBox.Width / ImagePictureBox.Image.Width
+        Dim scaleFactorY = ImagePictureBox.Height / ImagePictureBox.Image.Height
+
+        For Each face In _facesAnalysisResult.Faces
+            DrawFace(e, scaleFactorX, scaleFactorY, face)
+        Next
+
+    End Sub
+    
+    Private Sub DrawFace(e As PaintEventArgs, scaleFactorX As Double, scaleFactorY As Double, face As Face)
+
+        Dim rectangle = New Drawing.Rectangle(face.FaceRectangle.Left * scaleFactorX, 
+                                              face.FaceRectangle.Top * scaleFactorY, 
+                                              face.FaceRectangle.Width * scaleFactorX, 
+                                              face.FaceRectangle.Height * scaleFactorY)
+
+        Using pen = New Pen(Drawing.Color.Red, 4)
+            e.Graphics.DrawRectangle(pen, rectangle)
+        End Using
+
+        Dim caption = $"{face.Age} - {face.Gender}"
+        Dim yellowBrush = New SolidBrush(Drawing.Color.Yellow)
+        e.Graphics.DrawString(caption, New Font("Arial", 12), yellowBrush, rectangle)
+    End Sub
+
+End Class
